3ee3f488d70ce1db10e1e533503d0b57095547d0
dimenatar <71691899+dimenatar@users.noreply.github.com>
2020-11-12 20:25:39 +0300

    Added simple Encrypt and Decrypt

---
+++ Shifr/Shifr/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Shifr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(101, 106);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 96);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(593, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Подтвердить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(122, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Дешифрование";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 64);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(106, 20);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Шифрование";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCoral;
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel1.Location = new System.Drawing.Point(593, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(151, 174);
+            this.panel1.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"Текстовые файлы (*.txt)|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Shifr/Shifr/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Shifr
+{
+    public partial class Form1 : Form
+    {
+
+            static public string text;
+            static char[] Z = new char[12];
+            static char[] a = new char[12];
+            static char[] SCH = new char[12];
+            static char[] I = new char[12];
+            static char[] T = new char[12];
+            static char[] A = new char[12];
+            static public string Encrypt()
+            {
+                if (text.Length < 72)
+                {
+                MessageBox.Show("Текст содержит меньше 72 символов. Недостающие символы будут автоматически забиты рандомом ( ͡° ͜ʖ ͡°)");
+                Random random = new Random();
+                text += " ";
+                while (text.Length < 72)
+                {
+                    text += (char)random.Next(0,127);
+                }
+                }
+                else if (text.Length > 72)
+                {
+                MessageBox.Show("Текст содержит больше 72 символов. Лишние будут обрезаны ( ͡° ͜ʖ ͡°)");
+                text = text.Substring(0, 72);
+                }
+                for (int i = 0; i < 12; i++)
+                {
+                    Z[i] = text[i];
+                }
+                for (int i = 12; i < 24; i++)
+                {
+                    a[i - 12] = text[i];
+                }
+                for (int i = 24; i < 36; i++)
+                {
+                    SCH[i - 24] = text[i];
+                }
+                for (int i = 36; i < 48; i++)
+                {
+                    I[i - 36] = text[i];
+                }
+                for (int i = 48; i < 60; i++)
+                {
+                    T[i - 48] = text[i];
+                }
+                for (int i = 60; i < 72; i++)
+                {
+                    A[i - 60] = text[i];
+                }
+                char[] finalchar = new char[72];
+                for (int i = 0; i < 12; i++)
+                {
+                    finalchar[i * 6] = a[i];
+                    finalchar[i * 6 + 1] = A[i];
+                    finalchar[i * 6 + 2] = Z[i];
+                    finalchar[i * 6 + 3] = I[i];
+                    finalchar[i * 6 + 4] = T[i];
+                    finalchar[i * 6 + 5] = SCH[i];
+                }
+
+                string output = new string(finalchar);
+            return output;
+            }
+        static public string Decrypt()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                a[i] = text[i*6];
+                A[i] = text[i*6+1];
+                Z[i] = text[i*6+2];
+                I[i] = text[i*6+3];
+                T[i] = text[i*6+4];
+                SCH[i] = text[i*6+5];
+            }
+            string output1 = new string(Z);
+            string output2 = new string(a);
+            string output3 = new string(SCH);
+            string output4 = new string(I);
+            string output5 = new string(T);
+            string output6 = new string(A);
+            return output1 + output2 + output3 + output4 + output5 + output6;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    text = File.ReadAllText(openFileDialog1.FileName);
+                    richTextBox1.Text = Decrypt();
+                    MessageBox.Show("Текст расшифрован успешно");
+                }
+
+            }
+            else if (radioButton2.Checked)
+            {
+                text = richTextBox1.Text;
+                File.WriteAllText("Зашифрованный текст.txt",Encrypt());
+                MessageBox.Show("Текст зашифрован\nИщите его в дебаге");
+            }
+            
+        }
+    }
+}
